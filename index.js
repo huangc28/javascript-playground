29ad4c4383f105fbd1d6c12ebfb60c8edff125dd
Chi-Heng Huang <bryan.chi.h@mig.me>
2015-08-10 19:56:46 +0800

    progress check on route handler

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var server = require('./server.js');
 var router = require('./router.js');
+var requestHandlers = require('./requestHandlers.js');
+// server.start();
+var handle = {};
+handle['/']       = requestHandlers.start;
+handle['/start']  = requestHandlers.start;
+handle['/upload'] = 
+
 server.start(router.start);
